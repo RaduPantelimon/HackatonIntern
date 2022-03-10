92d9e1a0c1f6f7e1a51a634879e5fde2130396dd
Radu Pantelimon <radu.bucur@uipath.com>
2022-03-10 12:42:40 +0200

    Added Console Project to test the game. Started working on the Game class constructor

--- BlokeeActivity/Blokee.Activities/Blokee.Activities/BlokeeLogic/Game.cs
+++ BlokeeActivity/Blokee.Activities/Blokee.Activities/BlokeeLogic/Game.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,29 @@ namespace Blokee
     public class Game
     {
         public const int playerCount = 4;
+        private int _nextPlayer;
+        private Player[] _players;
+
+        public Game(JObject gameProperties, int nextPlayer) {
+            //determine who's turn is next
+            if (nextPlayer >= playerCount || nextPlayer < 0)
+                throw new Exception("Next player is invalid. It needs to be a number between 0 and the total number of players");
+            _nextPlayer = nextPlayer;
+
+            //init board based on the game Properties received
+            Board board = Board.I;
+            board.RefreshBoard(gameProperties["gameBoard"].ToString());
+
+            //init players
+            _players = new Player[playerCount];
+            Enumerable.Range(0, playerCount)
+                .ToList()
+                .ForEach(
+                index =>
+                {
+                    bool[] pieceAvailability = Enumerable.Repeat(true, 21).ToArray();
+                    _players[index] = new Player(index, pieceAvailability);
+                });
+        }
     }
 }
---
+++ BlokeeActivity/Blokee.Activities/TestConsoleApplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blokee;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace TestConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read the file as one string.
+            string text = System.IO.File.ReadAllText(@"test resources\latestValues.txt");
+            System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+            JObject gameProperties = JObject.Parse(text);
+
+            //init a new game
+            Game game = new Game(gameProperties,0);
+
+        }
+    }
+}
